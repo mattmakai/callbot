c79c0d8fe0a8d935dffc1df4d3128a9274d68c50
Matt Makai <matthew.makai@gmail.com>
2016-05-21 16:56:35 -0700

    that should do it for callbot

--- callbot.py
+++ callbot.py
@@ -21,6 +21,11 @@ twilio_client = TwilioRestClient()
 
 
 def handle_command(command, channel):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
     response = "Not sure what you mean. Use the *" + \
                CALL_COMMAND + "* command with numbers, delimited by spaces."
     if command.startswith(CALL_COMMAND):
@@ -74,12 +79,18 @@ def validate_phone_numbers(phone_numbers):
 
 
 def parse_slack_output(slack_rtm_output):
-    output = slack_rtm_output
-    if output and 'text' in output[0]:
-        if AT_BOT in output[0]['text']:
-            # return text after the @ mention, whitespace removed
-            return output[0]['text'].split(AT_BOT)[1].strip(), \
-                   output[0]['channel']
+    """
+        The Slack Real Time Messaging API is a firehose of data, so
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip(), \
+                       output['channel']
     return None, None
 
 
